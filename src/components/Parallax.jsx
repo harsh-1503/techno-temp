1b45eac1de8dca7a76d06ce818e008174ba896ef
Swarada Bhosale <swaradapb113@gmail.com>
2023-03-19 17:29:06 +0530

    Added different parallax effects

--- src/components/Parallax.jsx
+++ src/components/Parallax.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { Parallax } from 'react-parallax'
 import bgone from '../images/bg5.webp'
 import bgtwo from '../images/bg4.webp'
-import bgthree from '../images/bg3.jpg'
+import bgthree from '../images/bg2.jpg'
 import './parallax.css'
 
 const ParallaxEffect = () => {
@@ -12,17 +12,22 @@ const ParallaxEffect = () => {
     <>
       <Parallax strength={1000} bgImage={bgone} bgImageAlt='the cat'>
         <div className='content'>
-          <div className='text_content'>TEXT CONTENT</div>
+          <div className='text_content'>Normal Parallax</div>
         </div>
       </Parallax>
-      <Parallax strength={1000} bgImage={bgtwo} bgImageAlt='the cat'>
+      <Parallax
+        strength={1000}
+        blur={{ min: -5, max: 15 }}
+        bgImage={bgtwo}
+        bgImageAlt='the cat'
+      >
         <div className='content'>
-          <div className='text_content'>TEXT CONTENT</div>
+          <div className='text_content'>Blur Parallax</div>
         </div>
       </Parallax>
-      <Parallax strength={1000} bgImage={bgthree} bgImageAlt='the cat'>
+      <Parallax strength={-1000} bgImage={bgthree} bgImageAlt='the cat'>
         <div className='content'>
-          <div className='text_content'>TEXT CONTENT</div>
+          <div className='text_content'>Reverse Parallax</div>
         </div>
       </Parallax>
       <div className='content'></div>
