c5ac33b299f4e12ba7e3992bb29b26e33e1ce7c8
Swarada Bhosale <swaradapb113@gmail.com>
2023-04-01 00:03:10 +0530

    Created new about section cards

--- src/components/AboutEvent.js
+++ src/components/AboutEvent.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './AboutEvent.css'
-import { BsPatchCheckFill } from 'react-icons/bs'
+import ycircle from '../images/yellowcircle.png'
+import bcircle from '../images/bluecircle.png'
 
 function AboutEvent() {
+  const [hoveredf, setHoveredF] = useState(false)
+  const [hoveredb, setHoveredB] = useState(false)
   return (
     <section id='experience'>
       <h1 className='font-semibold'>About The Event</h1>
-      <h4 className=' mb-10 font-medium text-[1.2rem]'>
+      <h4
+        className=' mb-10 font-medium text-[1.2rem]'
+        style={{ color: 'rgb(64, 64, 64)' }}
+      >
         Lorem Ipsum is simply dummy text of the printing and typesetting
         industry. Lorem Ipsum has been the industry's standard dummy text ever
         since the 1500s, when an unknown printer took a galley of type and
@@ -19,83 +25,55 @@ function AboutEvent() {
       </h4>
 
       <div className='experienced_container '>
-        <div className='frontend_container pt-4 pb-8 shadow-xl'>
+        <div
+          className='frontend_container pt-4 pb-12 shadow-xl'
+          onMouseOver={() => setHoveredF(true)}
+          onMouseOut={() => setHoveredF(false)}
+        >
           <div className='frontend_heading'>
             <h3 className='font-semibold mb-4'>Frontend Development</h3>
           </div>
-          <div className='frontend_stack'>
-            <div className='frontend_stack_content'>
-              <BsPatchCheckFill />
-              <div className='iconless'>
-                <h4 className='ml-2 text-[1.2rem]'>HTML</h4>
-              </div>
+          <div className='stack'>
+            <div className='circle-container'>
+              <img src={hoveredf ? ycircle : bcircle} alt='circle' />
+              <img src={hoveredf ? ycircle : bcircle} alt='circle' />
+              <img src={hoveredf ? ycircle : bcircle} alt='circle' />
             </div>
-            <div className='frontend_stack_content'>
-              <BsPatchCheckFill />
-              <div className='iconless'>
-                <h4 className='ml-2 text-[1.2rem]'>CSS</h4>
-              </div>
-            </div>
-            <div className='frontend_stack_content'>
-              <BsPatchCheckFill />
-              <div className='iconless'>
-                <h4 className='ml-2 text-[1.2rem]'>JavaScript</h4>
-              </div>
-            </div>
-            <div className='frontend_stack_content'>
-              <BsPatchCheckFill />
-              <div className='iconless'>
-                <h4 className='ml-2 text-[1.2rem]'>React JS</h4>
-              </div>
-            </div>
-            <div className='frontend_stack_content'>
-              <BsPatchCheckFill />
-              <div className='iconless'>
-                <h4 className='ml-2 text-[1.2rem]'>BootStrap</h4>
-              </div>
-            </div>
-            <div className='frontend_stack_content'>
-              <BsPatchCheckFill />
-              <div className='iconless'>
-                <h4 className='ml-2 text-[1.2rem]'>Tailwind</h4>
-              </div>
+            <div className='about'>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Reprehenderit, vitae provident magnam ut blanditiis deleniti
+                fugiat vero culpa incidunt laborum Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Ducimus, blanditiis. Quaerat odio
+                temporibus optio numquam tenetur molestias, nostrum blanditiis
+                modi!
+              </p>
             </div>
           </div>
         </div>
-        <div className='backend_container pt-4 pb-8'>
+        <div
+          className='backend_container pt-4 pb-8'
+          onMouseOver={() => setHoveredB(true)}
+          onMouseOut={() => setHoveredB(false)}
+        >
           <div className='backend_heading'>
             <h3 className='font-semibold mb-4'>Backend Development</h3>
           </div>
-          <div className='backend_stack'>
-            <div className='backend_stack_content'>
-              <BsPatchCheckFill />
-              <div className='iconless'>
-                <h4 className='ml-2 text-[1.2rem]'>Node JS</h4>
-              </div>
+          <div className='stack'>
+            <div className='circle-container'>
+              <img src={hoveredb ? ycircle : bcircle} alt='circle' />
+              <img src={hoveredb ? ycircle : bcircle} alt='circle' />
+              <img src={hoveredb ? ycircle : bcircle} alt='circle' />
             </div>
-            <div className='backend_stack_content'>
-              <BsPatchCheckFill />
-              <div className='iconless'>
-                <h4 className='ml-2 text-[1.2rem]'>MongoDB</h4>
-              </div>
-            </div>
-            <div className='backend_stack_content'>
-              <BsPatchCheckFill />
-              <div className='iconless'>
-                <h4 className='ml-2 text-[1.2rem]'>MySQL</h4>
-              </div>
-            </div>
-            <div className='backend_stack_content'>
-              <BsPatchCheckFill />
-              <div className='iconless'>
-                <h4 className='ml-2 text-[1.2rem]'>PHP</h4>
-              </div>
-            </div>
-            <div className='backend_stack_content'>
-              <BsPatchCheckFill />
-              <div className='iconless'>
-                <h4 className='ml-2 text-[1.2rem]'>Python</h4>
-              </div>
+            <div className='about'>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Reprehenderit, vitae provident magnam ut blanditiis deleniti
+                fugiat vero culpa incidunt laborum Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Ducimus, blanditiis. Quaerat odio
+                temporibus optio numquam tenetur molestias, nostrum blanditiis
+                modi!
+              </p>
             </div>
           </div>
         </div>
