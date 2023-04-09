13a6fc82de9868860088ac063aa1261ec6fe38cd
harsh-1503 <harshkarwa1345@gmail.com>
2023-04-10 02:36:28 +0530

    updated images

--- src/API/registrationAPI.js
+++ src/API/registrationAPI.js
@@ -1,12 +1,17 @@
 import axios from "axios";
 import swal from "sweetalert2";
+import './alerts.css'
+
+// import { setGlobalState,useGlobalState } from "../components/Registration";
 const API = axios.create({
   baseURL: "https://technobackend-production.up.railway.app/",
 });
 export const getData = async (formData) => {
   try {
     // console.log(formData);
+    // setGlobalState(true);
     const res = await API.post("/getData", formData);
+    // setGlobalState(false);
     console.log("res :");
     console.log(res);
     console.log(res.data.postdata.phone.toString().length);
@@ -14,14 +19,14 @@ export const getData = async (formData) => {
       swal.fire({
         title: "Invalid Phone Number",
         imageUrl:
-          "https://res.cloudinary.com/dizrxbb27/image/upload/v1680810676/TechnoTweet/oops_qrmge0.png",
-        imageHeight: 300,
-        imageWidth: 200,
+          "https://res.cloudinary.com/dizrxbb27/image/upload/v1681066890/TechnoTweet/oops_qo58xk.png",
+        // imageHeight: 200,
+        // imageWidth: 200,
         confirmButtonColor: "#3085d6",
         confirmButtonText: "OK",
         animation: "true",
         customClass: {
-          popup: "animated fadeInDown faster",
+          popup: "alert-box animated fadeInDown faster",
           confirmButton: "animated bounceIn faster",
           cancelButton: "animated bounceIn faster",
         },
@@ -38,7 +43,7 @@ export const getData = async (formData) => {
           // cancelButtonColor: '#d33',
           confirmButtonText: "Continue",
           imageUrl:
-            "https://res.cloudinary.com/dizrxbb27/image/upload/v1680810621/TechnoTweet/hurray_crmrti.png",
+            "https://res.cloudinary.com/dizrxbb27/image/upload/v1681066882/TechnoTweet/hurray_uptaef.png",
           imageHeight: 200,
           imageWidth: 300,
           // animation: 'true',
@@ -66,10 +71,10 @@ export const getData = async (formData) => {
       err.response.data.message === "Email Already Registered"
     ) {
       swal.fire({
-        title: "Email already Registered!!!",
+        title: "Email already registered!!! Try using different email.",
         imageUrl:
-          "https://res.cloudinary.com/dizrxbb27/image/upload/v1680810676/TechnoTweet/oops_qrmge0.png",
-        imageHeight: 300,
+          "https://res.cloudinary.com/dizrxbb27/image/upload/v1681066890/TechnoTweet/oops_qo58xk.png",
+        imageHeight: 200,
         imageWidth: 200,
         confirmButtonColor: "#3085d6",
         confirmButtonText: "OK",
@@ -88,8 +93,8 @@ export const getData = async (formData) => {
       swal.fire({
         title: "Transaction ID already used. Try different one!!",
         imageUrl:
-          "https://res.cloudinary.com/dizrxbb27/image/upload/v1680810676/TechnoTweet/oops_qrmge0.png",
-        imageHeight: 300,
+          "https://res.cloudinary.com/dizrxbb27/image/upload/v1681066890/TechnoTweet/oops_qo58xk.png",
+        imageHeight: 200,
         imageWidth: 200,
         confirmButtonColor: "#3085d6",
         confirmButtonText: "OK",
--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,348 +1,238 @@
-import React, { useState } from 'react'
-import './Registration.css'
-import img1 from '../images/149qr.png'
-import img2 from '../images/179qr.png'
-import img3 from '../images/form1-removebg-preview.png'
-import hurrayImg from '../images/hurray.png'
+import React, { useState } from "react";
+import "./Registration.css";
+import img1 from "../images/149qr.png";
+import img2 from "../images/179qr.png";
+import img3 from "../images/form1-removebg-preview.png";
+import hurrayImg from "../images/hurray.png";
+import { createGlobalState } from "react-hooks-global-state";
 // import oops from '../images/oops.png'
 // import swal from 'sweetalert'
-import * as API from '../API/registrationAPI'
+import * as API from "../API/registrationAPI";
 // import Spinner from "react-bootstrap/Spinner";
-import '../fonts/font.css'
-import './inputs.css'
+import "../fonts/font.css";
+import "./inputs.css";
 // import { MDBSpinner } from "mdb-react-ui-kit";
+import { ColorRing } from "react-loader-spinner";
 
+// const {setGlobalState,useGlobalState} = createGlobalState(false);
 function Registration() {
-  const [img, setimg] = useState(img1)
-  const [domain, setDomain] = useState('Frontend')
-  const [loading, setloading] = useState(false)
+  const [img, setimg] = useState(img1);
+  // const isLoading = useGlobalState(false);
+  const [isLoading, setisLoading] = useState(false);
+  const [domain, setDomain] = useState("Frontend");
+  // const [loading, setloading] = useState(false);
   // const [hurray, sethurray] = useState(false)
   // const [oops, setoops] = useState(false)
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    phoneno: '',
-    transactionId: '',
-    collegeName: '',
-    yearOfStudy: 'First year',
-    course: 'Frontend',
-    isDualBooted: '',
-  })
+    name: "",
+    email: "",
+    phoneno: "",
+    transactionId: "",
+    collegeName: "",
+    yearOfStudy: "First year",
+    course: "Frontend",
+    isDualBooted: "",
+  });
 
   const handleChange = (event) => {
-    event.preventDefault()
-    const { name, value } = event.target
+    event.preventDefault();
+    const { name, value } = event.target;
     setFormData((prevFormData) => ({
       ...prevFormData,
       [name]: value,
-    }))
-    console.log(formData)
-  }
+    }));
+    console.log(formData);
+  };
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    setloading(true)
-    const data = API.getData(formData)
-    setloading(false)
+  const handleSubmit =  (e) => {
+    e.preventDefault();
+    setisLoading(true);
+    const data =  API.getData(formData);
+    setisLoading(false);
     // sethurray(true)
-  }
+  };
 
   const handleQR = (e) => {
     // e.preventDefault();
-    const courseSelector = document.getElementById('inputGroupSelect01')
-    if (courseSelector.value === 'Frontend') {
-      setimg(img1)
-      setDomain('Frontend')
-    } else if (courseSelector.value === 'Backend') {
-      setimg(img1)
-      setDomain('Backend')
-    } else if (courseSelector.value === 'Both') {
-      setimg(img2)
-      setDomain('Both')
+    const courseSelector = document.getElementById("inputGroupSelect01");
+    if (courseSelector.value === "Frontend") {
+      setimg(img1);
+      setDomain("Frontend");
+    } else if (courseSelector.value === "Backend") {
+      setimg(img1);
+      setDomain("Backend");
+    } else if (courseSelector.value === "Both") {
+      setimg(img2);
+      setDomain("Both");
     }
-    handleChange(e)
-  }
+    handleChange(e);
+  };
 
   return (
     <>
-      <div className='whole_form w-full mt-36 p-1 w-lg-50' id='register'>
-        <div className='upperLower'>
-          <div className='upperDiv'>
-            <h2 className='registration text-center text-warning h1 font-semibold m-4'>
+      <div className="whole_form w-full mt-36 p-1 w-lg-50" id="register">
+        <div className="upperLower">
+          <div className="upperDiv">
+            <h2 className="registration text-center text-warning h1 font-semibold m-4">
               Registration
             </h2>
           </div>
 
-          <div className='lowerdiv'>
-            <div className='img_qr_one'>
-              <h5 className='text-center font-weight-bold my-4 mx-4'>
+          <div className="lowerdiv">
+            <div className="img_qr_one">
+              <h5 className="text-center font-weight-bold my-4 mx-4">
                 Scan this QR code to make the payment
               </h5>
-              <h3 className='text-center font-weight-bold my-4 mx-4'>
+              <h3 className="text-center font-weight-bold my-4 mx-4">
                 {domain}
               </h3>
-              <div className='register-imgs'>
+              <div className="register-imgs">
                 <img
                   src={img}
-                  className='QR rounded mx-auto w-1/4 sm:w-1/2 mb-12'
-                  alt='...'
+                  className="QR rounded mx-auto w-1/4 sm:w-1/2 mb-12"
+                  alt="..."
                 />
-                <div className='end_image'>
-                  <img src={img3} alt='' className='mx-auto' />
+                <div className="end_image">
+                  <img src={img3} alt="" className="mx-auto" />
                 </div>
               </div>
             </div>
-            {/* <form className='form' onSubmit={handleSubmit}>
-              <div className='input-flex form-group border-none flex my-4 sm:my-1 input_container'>
-                <label for='exampleInputPassword1' className='basis-1/3'>
-                  Name:
-                </label>
+            <form className="form" onSubmit={handleSubmit}>
+              <div className="inputs">
+                <label for="exampleInputPassword1">Name</label>
                 <input
-                  type='text'
-                  name='name'
+                  type="text"
+                  name="name"
                   value={formData.name}
                   onChange={handleChange}
-                  className='form-control  h-11 basis-2/3 rounded-lg bg-[#44c8fc] '
-                  id='exampleInputPassword1'
-                  autoComplete='off'
+                  id="exampleInputPassword1"
+                  autoComplete="off"
                   required
+                  placeholder="Full Name"
                 />
-              </div>
-              <div className='input-flex form-group border-none flex my-4 sm:my-1 input_container'>
-                <label for='exampleInputEmail1' className='basis-1/3'>
-                  Email ID:
-                </label>
+                <label for="exampleInputEmail1">Email ID</label>
                 <input
-                  type='email'
-                  name='email'
+                  type="email"
+                  name="email"
                   value={formData.email}
                   onChange={handleChange}
-                  className='form-control   h-11 basis-2/3 rounded-lg bg-[#44c8fc]'
-                  id='exampleInputEmail1'
-                  autoComplete='off'
+                  id="exampleInputEmail1"
+                  autoComplete="off"
                   required
+                  placeholder="Email"
                 />
-              </div>
-
-              <div className='input-flex form-group border-none flex  my-4 input_container'>
-                <label for='collegeName' className='basis-1/3'>
-                  Phone No:
-                </label>
+                <label for="collegeName">Phone No</label>
                 <input
-                  type='number'
-                  name='phoneno'
+                  type="tel"
+                  name="phoneno"
                   value={formData.phoneno}
                   onChange={handleChange}
-                  className='form-control   h-11 basis-2/3 rounded-lg bg-[#44c8fc]'
-                  id='collegeName'
-                  aria-describedby='emailHelp'
-                  autoComplete='off'
+                  id="collegeName"
+                  aria-describedby="emailHelp"
+                  autoComplete="off"
                   required
+                  placeholder="9876543210"
                 />
-              </div>
-
-              <div className='input-flex form-group border-none flex my-4 sm:my-1 input_container'>
-                <label for='collegeName' className='basis-1/3'>
-                  College Name:
-                </label>
+                <label for="collegeName">College Name</label>
                 <input
-                  type='text'
-                  name='collegeName'
-                  className='form-control h-11 basis-2/3 rounded-lg bg-[#44c8fc]'
-                  id='collegeName'
+                  type="text"
+                  name="collegeName"
+                  id="collegeName"
                   value={formData.collegeName}
                   onChange={handleChange}
-                  aria-describedby='emailHelp'
-                  autoComplete='off'
+                  aria-describedby="emailHelp"
+                  autoComplete="off"
                   required
+                  placeholder="Walchand College of Engineering, Sangli"
                 />
-              </div>
-              <div className='input-flex input-group mb-3 my-4 sm:my-1  input_container'>
-                <label className='basis-1/3' for='yearOfStudy'>
-                  Year of Study:
-                </label>
+                <label for="yearOfStudy">Year of Study</label>
                 <select
-                  className='Select basis-2/3 p-2 lg:p-0 rounded-lg bg-[#44c8fc] '
-                  id='yearOfStudy'
-                  name='yearOfStudy'
+                  id="yearOfStudy"
+                  name="yearOfStudy"
                   value={formData.yearOfStudy}
                   onChange={handleChange}
                   required
+                  placeholder="First Year"
+                  className="Select"
                 >
-                  <option selected value='First Year'>
+                  <option selected value="First Year">
                     First Year
                   </option>
-                  <option value='Second Year'>Second Year</option>
-                  <option value='Third Year'>Third Year</option>
-                  <option value='Fourth Year'>Fourth Year</option>
+                  <option value="Second Year">Second Year</option>
+                  <option value="Third Year">Third Year</option>
+                  <option value="Fourth Year">Fourth Year</option>
                 </select>
-              </div>
-
-              <div className='input-flex input-group mb-3 my-4 sm:my-1 input_container'>
-                <label className='basis-1/3' for='inputGroupSelect01'>
-                  Select Course:
-                </label>
+                <label for="inputGroupSelect01">Select Course </label>
                 <select
-                  className='Select basis-2/3 p-2 lg:p-0 rounded-lg bg-[#44c8fc] '
-                  id='inputGroupSelect01'
-                  name='course'
+                  id="inputGroupSelect01"
+                  name="course"
                   value={formData.course}
                   onChange={handleQR}
+                  className="Select"
                   required
                 >
                   <option selected>Both</option>
                   <option>Frontend</option>
                   <option>Backend</option>
                 </select>
-              </div>
-              <div className='input-flex form-group border-none flex input_container transaction mb-14'>
-                <label for='paymentID' className='basis-1/3 '>
-                  Transaction ID:
-                </label>
-                <input
-                  type='text'
-                  name='transactionId'
-                  value={formData.transactionId}
-                  onChange={handleChange}
-                  className='form-control  h-11 basis-2/3 rounded-lg bg-[#44c8fc]'
-                  id='paymentID'
-                  aria-describedby='emailHelp'
-                  autoComplete='off'
-                  required
-                />
-              </div>
-              <div className='btn_class '>
-                <button
-                  type='submit'
-                  name='submit'
-                  className='register_btn text-center'
-                >
-                  Register
-                </button>
-              </div>
-            </form> */}
-            <form className='form' onSubmit={handleSubmit}>
-              <div className='inputs'>
-                <label for='exampleInputPassword1'>Name</label>
-                <input
-                  type='text'
-                  name='name'
-                  value={formData.name}
-                  onChange={handleChange}
-                  id='exampleInputPassword1'
-                  autoComplete='off'
-                  required
-                  placeholder='Full Name'
-                />
-                <label for='exampleInputEmail1'>Email ID</label>
-                <input
-                  type='email'
-                  name='email'
-                  value={formData.email}
-                  onChange={handleChange}
-                  id='exampleInputEmail1'
-                  autoComplete='off'
-                  required
-                  placeholder='Email'
-                />
-                <label for='collegeName'>Phone No</label>
-                <input
-                  type='tel'
-                  name='phoneno'
-                  value={formData.phoneno}
-                  onChange={handleChange}
-                  id='collegeName'
-                  aria-describedby='emailHelp'
-                  autoComplete='off'
-                  required
-                  placeholder='9876543210'
-                />
-                <label for='collegeName'>College Name</label>
-                <input
-                  type='text'
-                  name='collegeName'
-                  id='collegeName'
-                  value={formData.collegeName}
-                  onChange={handleChange}
-                  aria-describedby='emailHelp'
-                  autoComplete='off'
-                  required
-                  placeholder='Walchand College of Engineering, Sangli'
-                />
-                <label for='yearOfStudy'>Year of Study</label>
-                <select
-                  id='yearOfStudy'
-                  name='yearOfStudy'
-                  value={formData.yearOfStudy}
-                  onChange={handleChange}
-                  required
-                  placeholder='First Year'
-                  className='Select'
-                >
-                  <option selected value='First Year'>
-                    First Year
-                  </option>
-                  <option value='Second Year'>Second Year</option>
-                  <option value='Third Year'>Third Year</option>
-                  <option value='Fourth Year'>Fourth Year</option>
-                </select>
-                <label for='inputGroupSelect01'>Select Course </label>
-                <select
-                  id='inputGroupSelect01'
-                  name='course'
-                  value={formData.course}
-                  onChange={handleQR}
-                  className='Select'
-                  required
-                >
-                  <option selected>Both</option>
-                  <option>Frontend</option>
-                  <option>Backend</option>
-                </select>
-                <label for='paymentID' className='basis-1/3 '>
+                <label for="paymentID" className="basis-1/3 ">
                   Transaction ID
                 </label>
                 <input
-                  type='text'
-                  name='transactionId'
+                  type="text"
+                  name="transactionId"
                   value={formData.transactionId}
                   onChange={handleChange}
-                  id='paymentID'
-                  aria-describedby='emailHelp'
-                  autoComplete='off'
+                  id="paymentID"
+                  aria-describedby="emailHelp"
+                  autoComplete="off"
                   required
                 />
 
                 <button
-                  type='submit'
+                  type="submit"
                   style={{
-                    color: 'black',
-                    backgroundColor: '#ffc107',
-                    fontWeight: 'bold',
-                    borderRadius: '0.7rem', 
-                    height: '2.5rem',
+                    color: "black",
+                    backgroundColor: "#ffc107",
+                    fontWeight: "bold",
+                    borderRadius: "0.7rem",
+                    display:"flex",
+                    flexDirection:"row",
+                    justifyContent:'center',
+                    alignItems:'center',
                   }}
                 >
-                  Register
-                  {/* {loading && <div className="loading-spinner"></div>} */}
+                  <div>Register</div>
+                  <div>{isLoading && (
+                    <ColorRing
+                      visible={true}
+                      // height="60"
+                      width="40"
+                      ariaLabel="blocks-loading"
+                      wrapperStyle={{}}
+                      wrapperClass="blocks-wrapper"
+                      colors={["#000000"]}
+                    />
+                  )}</div>
+                  
                 </button>
               </div>
             </form>
-            <div className='img_qr_two'>
-              <h5 className='text-center font-weight-bold my-4 mx-4'>
+            <div className="img_qr_two">
+              <h5 className="text-center font-weight-bold my-4 mx-4">
                 Scan this QR code to make the payment
               </h5>
-              <h3 className='text-center font-weight-bold my-4 mx-4'>
+              <h3 className="text-center font-weight-bold my-4 mx-4">
                 {domain}
               </h3>
-              <div className='register-imgs'>
+              <div className="register-imgs">
                 <img
                   src={img}
-                  className='QR rounded mx-auto w-1/4 sm:w-1/2 mb-12'
-                  alt='...'
+                  className="QR rounded mx-auto w-1/4 sm:w-1/2 mb-12"
+                  alt="..."
                 />
-                <div className='end_image'>
-                  <img src={img3} alt='' className='mx-auto' />
+                <div className="end_image">
+                  <img src={img3} alt="" className="mx-auto" />
                 </div>
               </div>
             </div>
@@ -350,7 +240,8 @@ function Registration() {
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Registration
+export default Registration;
+// export {useGlobalState,setGlobalState}
