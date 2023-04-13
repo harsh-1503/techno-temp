39b5021cb6bd8f9dd5c4bef85087acf1158ab75a
Swarada Bhosale <swaradapb113@gmail.com>
2023-04-13 23:23:10 +0530

    Modified sponsors file

--- src/components/Sponsor.js
+++ src/components/Sponsor.js
@@ -4,12 +4,12 @@ import sponsorLogo from '../images/CodeCrafters.io full logo (Dark text).png'
 
 const Sponsor = () => {
   return (
-    
-      <div className='sponsors'>
-        <h1 className='backed'>Backed by:</h1>
-        <img src={sponsorLogo} alt='...'/>
-      </div>
-    
+    <div className='sponsors'>
+      <h1 className='backed'>Boosted by:</h1>
+      <a href='https://codecrafters.io/' target='_blank'>
+        <img src={sponsorLogo} alt='...' />
+      </a>
+    </div>
   )
 }
 
